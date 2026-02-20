5d6035aa2b1b6e3244f81bdb2b57402cf239b650
Nathan <209938737+quiteboring@users.noreply.github.com>
2026-02-19 19:17:11 -0500

    chore: initialize project

---
+++ src/main/kotlin/org/cobalt/unit/Unit.kt
@@ -0,0 +1,11 @@
+package org.cobalt.unit
+
+import net.fabricmc.api.ClientModInitializer
+
+class Unit : ClientModInitializer{
+
+  override fun onInitializeClient() {
+    println("Hello World!")
+  }
+
+}
